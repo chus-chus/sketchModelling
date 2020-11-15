91efece0cab023f09f44d650893f3e931ac874e8
Chus Antoñanzas <jemanac1998@gmail.com>
2020-11-15 21:13:31 +0100

    Variance EH working, not super tested.

--- src/sketches/EH.py
+++ src/sketches/EH.py
@@ -75,6 +75,7 @@ class BinaryCounterEH(object):
             if numberOfSameCount == self.bucketThreshold:
                 formerBucket.nElems += latterBucket.nElems
                 del self.buckets[bucketIndex]
+                # todo revise, indexes do change
                 # if buckets are merged, next element of deque moves into
                 # the position of the deleted bucket. So, index does not change,
                 # as it now points to a latter bucket and formerBucket is updated
@@ -120,7 +121,6 @@ class SumEH(BinaryCounterEH):
         self.buffer.appendleft(Bucket(timestamp, number))
         self.bufferSum += number
         # todo remove expired buckets every n events even if numbers arriving are 0
-        # todo check if maxBufferSize refers to sum or nElems
         # todo check problems if a lot of elems are 0 (keeps the mean until a non-zero elem arrives)
         if len(self.buffer) == self.maxBufferSize:
             self.total += self.bufferSum
@@ -273,12 +273,20 @@ class VarBucket(Bucket):
     """ An extension of the basic bucket that also contains its mean and its variance. """
 
     def __init__(self, timestamp, value):
-        super().__init__(timestamp, 1)
-        self.bucketMean = value
+        # Indicate value=None if the Bucket is to be initialized empty
+        if value is None:
+            super().__init__(timestamp, 0)
+            self.bucketMean = 0
+        else:
+            super().__init__(timestamp, 1)
+            self.bucketMean = value
+
         self.var = 0
 
 
 class Counter(object):
+
+    """ A simple wraparound counter. """
 
     def __init__(self, upperLimit):
         self.step = 0
@@ -292,6 +300,10 @@ class Counter(object):
             self.step = 1
 
     def dist_between_ticks(self, tick1, tick2):
+
+        """ Returns the number of steps that have taken place between 'tick1' and 'tick2' assuming that at most 1
+        wraparound has occured. """
+
         if tick1 <= tick2:
             return tick2 - tick1
         else:
@@ -299,93 +311,113 @@ class Counter(object):
 
 
 class VarEH(object):
-    # todo review timestamp
-    # todo test merging procedure
+
+    """ A variation of the original EH structure that keeps track of the variance (k-medians with k = 1) to some eps
+    of relative error and mean in a window of length n. Although less space-efficient than the original, it still is
+    sublinear to window length and provides approximations in constant time. Moreover, it is more flexible in terms of
+    the functions it supports and can work with real numbers. Amortized update time is O(1) given that the |max. value|
+    of the data is known a priori.
+
+    Note that although the guarantees for the maximum relative error for the estimate of the mean are not presented in
+    the paper, but almost always hold experimentally. # todo test thoroughly"""
 
     def num_elements(self):
+
+        """ Returns the number of elements (expired and not expired) contained within the structure. """
+
         nElems = 0
         for bucket in self.buckets:
             nElems += bucket.nElems
         return nElems
 
     def print_eh(self):
+
+        """ Prints buckets and their information from most recent to oldest (0). """
+
         for i in range(len(self.buckets)-1, -1, -1):
             print('Bucket ', i, '. nElems = ', self.buckets[i].nElems, ', mean = ', self.buckets[i].bucketMean,
-                  ', var = ', self.buckets[i].var, 'timestamp = ', self.buckets[i].timestamp)
+                  ', var = ', self.buckets[i].var, ', timestamp = ', self.buckets[i].timestamp)
 
     def __init__(self, n, eps, maxValue=None):
         self.n = n
         self.k = 9 / (eps ** 2)
         self.buckets = deque([])
-        self.lastSuffix = VarBucket(0, 0)
+        self.lastSuffix = VarBucket(0, None)
         self.interSuffix = None
         self.timeCounter = Counter(2*n)
 
-        # if resolution is not specified, then amortized running time per element will not be O(1)
+        # If resolution is not specified, then amortized running time per element will not be O(1)
         self.stepsBetweenMerges = int(round((1 / eps) * log2(n * (maxValue ** 2)))) if maxValue is not None else 1
-        # elements processed since last merge
+        # Elements processed since last merge
         self.stepsSinceLastMerge = 0
 
     def add(self, value):
-        # todo fix variance estimate overreaching
-        # note: timestamp up to 2*n arbitrary
-        """ Todo overview of procedure """
-        self.stepsSinceLastMerge += 1
-        self.timeCounter.increment()
-        self.insert_into_last_suffix(value)
 
-        # New element does not affect statistics
-        if self.buckets and value == self.buckets[-1].bucketMean:
-            self.buckets[-1].nElems += 1
+        # todo make max value of wraparaound timestamp consistent.
+        """ Process a new element arrival, updating the statistics of the structure. If the EH is empty, just insert it.
+            If there's at least one element:
+                1. Update B_m* (lastSuffix)
+                2. Insert the value (either in the previous or in a new Bucket)
+                3. If the last bucket is expired, delete it and update B_m* so that it represents B_(m-1)*
+                4. If 'stepsBetweenMerges' elements have arrived after the last bucket merge, merge buckets again. """
+
+        self.timeCounter.increment()
+        self.stepsSinceLastMerge += 1
+
+        if self.buckets:
+            # Maintain B_m*
+            self.insert_into_last_suffix(value)
+            # New element does not affect statistics
+            if value == self.buckets[-1].bucketMean:
+                self.buckets[-1].nElems += 1
+                self.buckets[-1].timestamp = self.timeCounter.step
+            else:
+                self.buckets.append(VarBucket(self.timeCounter.step, value))
         else:
             self.buckets.append(VarBucket(self.timeCounter.step, value))
+            return
 
         # Delete expired bucket, check on counter's wraparound property
-        # todo expired timestamp not being detected properly
         if self.get_position(self.buckets[0].timestamp) > self.n:
-            # update suffix
             self.pop_from_last_suffix()
             self.buckets.popleft()
 
-        # merge every self.stepsBetweenMerge steps to ensure amortized time O(1) (only if maxValue has been specified)
+        # Merge every self.stepsBetweenMerge steps to ensure amortized time O(1) (only if maxValue has been specified)
         if self.stepsSinceLastMerge == self.stepsBetweenMerges:
             self.merge_buckets()
             self.stepsSinceLastMerge = 0
 
     @staticmethod
     def compute_new_mean(bucket1, bucket2, nElems):
+
         """ Computes the mean of the combination of two buckets. """
+
         return (bucket1.bucketMean * bucket1.nElems + bucket2.bucketMean * bucket2.nElems) / nElems
 
     @staticmethod
     def compute_new_variance(bucket1, bucket2, nElems):
+
         """ Computes the variance of the combination of two buckets. """
+
         return (bucket1.var + bucket2.var + ((bucket1.nElems * bucket2.nElems) / nElems) *
                 ((bucket1.bucketMean - bucket2.bucketMean) ** 2))
-
-    def update_inter_suffix(self, index):
-        """ Updates the suffix bucket B_index* (see reference -> insert procedure -> step 3) such that it now also
-        contains the statistics of elements from bucket in position 'index'. That is, B_index* now
-        represents B_(index+1)*. When called, it assumes that the new elements to include have not been taken into
-        account yet. """
-        # Reminder: old buckets are in low indexes.
-        newNElems = self.interSuffix.nElems + self.buckets[index].nElems
-        self.interSuffix.bucketMean = self.compute_new_mean(self.buckets[index], self.interSuffix, newNElems)
-        self.interSuffix.var = self.compute_new_variance(self.buckets[index], self.interSuffix, newNElems)
-        self.interSuffix.nElems = newNElems
 
     def insert_into_last_suffix(self, element):
         """ Updates the statistics of the suffix bucket B_m* (in reference) such that it now takes
         into account another element. """
+        # order of operations crucial!
         newNElems = self.lastSuffix.nElems + 1
-        self.lastSuffix.bucketMean = (self.lastSuffix.bucketMean * self.lastSuffix.nElems + element) / newNElems
         self.lastSuffix.var = (self.lastSuffix.var + (self.lastSuffix.nElems / newNElems) *
                                ((self.lastSuffix.bucketMean - element) ** 2))
+        self.lastSuffix.bucketMean = (self.lastSuffix.bucketMean * self.lastSuffix.nElems + element) / newNElems
         self.lastSuffix.nElems = newNElems
 
     def pop_from_last_suffix(self):
+
         """ Updates the statistics of the suffix bucket B_m* (in reference) such that it does not take
          into account the oldest bucket anymore: it now represents B_(m-1)* """
+
+        # order of operations crucial!
         newNElems = self.lastSuffix.nElems - self.buckets[1].nElems
         self.lastSuffix.bucketMean = (self.lastSuffix.bucketMean * self.lastSuffix.nElems -
                                       self.buckets[1].bucketMean * self.buckets[1].nElems) / newNElems
@@ -395,41 +427,48 @@ class VarEH(object):
         self.lastSuffix.nElems = newNElems
 
     def get_var_estimate(self):
+
         """ Returns an estimate of the variance within the window. """
+
         numEst = self.n + 1 - self.get_position(self.buckets[0].timestamp)
         return (self.buckets[0].var / 2 + self.lastSuffix.var +
                 ((numEst * self.lastSuffix.nElems)/(numEst + self.lastSuffix.nElems)) *
                 ((self.buckets[0].bucketMean - self.lastSuffix.bucketMean)**2))
 
     def get_mean_estimate(self):
+
         """ Returns an estimate of the mean within the window. """
+
         numEst = self.n + 1 - self.get_position(self.buckets[0].timestamp)
         return (((numEst * self.buckets[0].bucketMean) +
                  (self.lastSuffix.nElems * self.lastSuffix.bucketMean)) /
                 (numEst + self.lastSuffix.nElems))
 
     def get_position(self, timestamp):
+
         """ Gets position of an element in the EH (from 1 to n) based on its timestamp. """
+
         return self.timeCounter.dist_between_ticks(timestamp, self.buckets[-1].timestamp) + 1
 
     def merge_buckets(self):
         # Merge buckets
         if len(self.buckets) > 2:
-            self.interSuffix = VarBucket(0, 0)
+            self.interSuffix = VarBucket(0, None)
             i = len(self.buckets) - 3
             j = i + 1
             newNElems = self.buckets[i].nElems + self.buckets[j].nElems
             newVar = self.compute_new_variance(self.buckets[i], self.buckets[j], newNElems)
             self.update_inter_suffix(len(self.buckets) - 1)
-            while i > 0:
+            # here
+            while i >= 0:
                 if self.k * newVar <= self.interSuffix.var:
                     self.buckets[i].bucketMean = self.compute_new_mean(self.buckets[i], self.buckets[j], newNElems)
                     self.buckets[i].nElems = newNElems
                     self.buckets[i].var = newVar
                     self.buckets[i].timestamp = self.buckets[j].timestamp
                     # Update intermediate suffix bucket before deleting the bucket
-                    self.update_inter_suffix(j)
                     del self.buckets[j]
+                    # with bucket deleted, j represents buckets[j+1] before deletion
                 else:
                     self.update_inter_suffix(j)
                 # Prepare for conditional check
@@ -438,3 +477,15 @@ class VarEH(object):
                 newNElems = self.buckets[i].nElems + self.buckets[j].nElems
                 newVar = self.compute_new_variance(self.buckets[i], self.buckets[j], newNElems)
 
+    def update_inter_suffix(self, index):
+
+        """ Updates the suffix bucket B_index* (see reference -> insert procedure -> step 3) such that it now also
+        contains the statistics of elements from bucket in position 'index'. That is, B_index* now
+        represents B_(index+1)*. When called, it assumes that the new elements to include have not been taken into
+        account yet. """
+
+        # Reminder: old buckets are in low indexes.
+        newNElems = self.interSuffix.nElems + self.buckets[index].nElems
+        self.interSuffix.bucketMean = self.compute_new_mean(self.buckets[index], self.interSuffix, newNElems)
+        self.interSuffix.var = self.compute_new_variance(self.buckets[index], self.interSuffix, newNElems)
+        self.interSuffix.nElems = newNElems
